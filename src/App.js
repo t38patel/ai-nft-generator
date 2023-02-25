334699a24ad1603f31e861df21f8b6af12bacb74
BuildTools <unconfigured@null.spigotmc.org>
2023-02-25 10:11:20 -0500

    Initial setup commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { NFTStorage, File } from 'nft.storage'
+import { Buffer } from 'buffer';
+import { ethers } from 'ethers';
+import axios from 'axios';
+
+// Components
+import Spinner from 'react-bootstrap/Spinner';
+import Navigation from './components/Navigation';
+
+// ABIs
+import NFT from './abis/NFT.json'
+
+// Config
+import config from './config.json';
+
+function App() {
+  const [provider, setProvider] = useState(null)
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
+  return (
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <p>Edit App.js to get started.</p>
+
+    </div>
+  );
+}
+
+export default App;
